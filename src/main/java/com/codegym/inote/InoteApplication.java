883fbd31f2dee0a7dc074506af23c8c7244ac91d
datquocngo <shaco14091999@gmail.com>
2019-01-09 08:13:56 +0700

    Initial commit

---
+++ src/main/java/com/codegym/inote/InoteApplication.java
@@ -0,0 +1,14 @@
+package com.codegym.inote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InoteApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/codegym/inote/InoteApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codegym.inote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InoteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
